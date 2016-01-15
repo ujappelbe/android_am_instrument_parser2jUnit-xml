2422b9a7e39d4081d3bc7be0d59e2d6ebd6e26f4
Jon Appelberg <jon@unity3d.com>
2016-01-15 10:48:02 +0200

    Added usage at top of file. Added support for option third param which specifies the name of the root test case

--- parseresults.py
+++ parseresults.py
@@ -1,15 +1,30 @@
+ #############################################################
+# This script will use am_instrument_parser from android SDK  #
+# to parse android am instruments test raw output into        #
+# junit.xml format.                                           #
+# Usage: python parseresults.py req1 req2 opt1                #
+#   req1: input raw file path                                 #
+#   req2: output junit.xml file path                          #
+#   opt1: name of Root- test suite (default "Root")           #
+ #############################################################
+
 import sys
+import re
 from am_instrument_parser import TestResult
 from am_instrument_parser import ParseAmInstrumentOutput
 
-if(len(sys.argv) != 3):
+if(len(sys.argv) < 3):
     print("Please provide inputFile and outputFile as arguments!")
     sys.exit()
 
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
+rootSuiteName = "Root"
+if(len(sys.argv) > 3):
+    rootSuiteName = re.sub('[^A-Za-z0-9\.\-_, ]+', ' ', sys.argv[3])
 
 print("Will read from file '" + inputFile + "' and output to '" + outputFile + "'" )
+print("Using '" + rootSuiteName + "' as test suite root name")
 
 with open(inputFile, "r") as myfile:
     data = myfile.read()
@@ -34,7 +49,7 @@ print("Amount of steps = " + str(len(testResults)))
 with open(outputFile, "w") as outfile:
     outfile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
     outfile.write("<testsuites name=\"Unit Tests\" tests=\"" + str(len(testResults)) + "\" failures=\"" + str(failures) + "\" skipped=\"" + str(skipped) + "\"" + ">\n")
-    outfile.write("\t<testsuite name=\"Aggregated\" tests=\"" + str(len(testResults)) + "\" failures=\"" + str(failures) + "\" skipped=\"" + str(skipped) + "\"" + ">\n")
+    outfile.write("\t<testsuite name=\"" + rootSuiteName + "\" tests=\"" + str(len(testResults)) + "\" failures=\"" + str(failures) + "\" skipped=\"" + str(skipped) + "\"" + ">\n")
 
     for result in testResults:
         outfile.write("\t\t<testcase name=\"" + str(result.GetTestName()) + "\">\n")
