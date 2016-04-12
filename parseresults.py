261100454ccdb6c2bb128a114a8fc0cd3e30ac13
Jon Appelberg <jon@unity3d.com>
2016-04-12 17:48:55 +0300

    Feature to stitch several raw android testrunner results together and turn them to one junit.xml file. I just noticed this feature probably is completely unnecessary since android testrunner supports this internally. Committing this anyway since the commanline arguments feature might be useful

--- parseresults.py
+++ parseresults.py
@@ -11,75 +11,124 @@
 import sys
 import re
 import string
+import argparse
 from am_instrument_parser import TestResult
 from am_instrument_parser import ParseAmInstrumentOutput
 
-if(len(sys.argv) < 3):
-    print("Please provide inputFile and outputFile as arguments!")
-    sys.exit()
 
-inputFile = sys.argv[1]
-outputFile = sys.argv[2]
-rootSuiteName = "Root"
-if(len(sys.argv) > 3):
-    rootSuiteName = re.sub('[^A-Za-z0-9\.\-_, ]+', '', sys.argv[3])
+def writeOutput(outfile, testResults, resultsDict):
+    # add the step that checks that all tests have been executed the
+    # the number of total test steps
+    totalNumTests = len(testResults) + 1
+    failures = resultsDict['failures']
+    skipped = resultsDict['skipped']
 
-print("Will read from file '" + inputFile + "' and output to '" +
+    with open(outputFile, "w") as outfile:
+        outfile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        outfile.write("<testsuites name=\"Unit Tests\" tests=\"" +
+                      str(totalNumTests) + "\" failures=\"" + str(failures) +
+                      "\" skipped=\"" + str(skipped) + "\"" + ">\n")
+        outfile.write("\t<testsuite name=\"" + rootSuiteName + "\" tests=\"" +
+                      str(totalNumTests) + "\" failures=\"" + str(failures) +
+                      "\" skipped=\"" + str(skipped) + "\"" + ">\n")
+
+        for result in testResults:
+            outfile.write("\t\t<testcase name=\"" + str(result.GetTestName()) +
+                          "\">\n")
+            if(result.GetStatusCode() < 0):
+                if(result.GetStatusCode() == -3):
+                    outfile.write("\t\t\t<skipped />\n")
+                else:
+                    outfile.write("\t\t\t<failure> <![CDATA[" +
+                                  str(result.GetFailureReason()) +
+                                  "]]></failure>\n")
+            outfile.write("\t\t</testcase>\n")
+        outfile.write("\t\t<testcase name=\"All tests were executed\">\n")
+        if(resultsDict['numTests'] != str(len(testResults))):
+            outfile.write("\t\t\t<failure> <![CDATA[ Expected '" + resultsDict['numTests'] +
+                          "' steps. Got results for '" +
+                          str(len(testResults)) + "'" "]]></failure>\n")
+        outfile.write("\t\t</testcase>\n")
+        outfile.write("\t</testsuite>\n")
+        outfile.write("</testsuites>\n")
+
+
+# Returns tuple of testResults-object and dict with result metrics
+def processRawResultsFile(inputFile):
+    with open(inputFile, "r") as myfile:
+        data = myfile.read()
+
+    numTests = "-1"
+    numTestsLine = -1
+    reNumTests = re.compile(r'INSTRUMENTATION_STATUS: numtests=(\d*)$')
+    for line in data.splitlines():
+        numTestsLine += 1
+        if(reNumTests.match(line)):
+            numTests = str(reNumTests.search(line).group(1))
+            break
+
+    output, bundle = ParseAmInstrumentOutput(data)
+    testResults = output
+    resultSum = {}
+
+    resultSum['failures'] = 0
+    resultSum['skipped'] = 0
+    resultSum['numTests'] = numTests
+    for result in testResults:
+        if(result.GetStatusCode() < 0):
+            if(result.GetStatusCode() == -3):
+                resultSum['skipped'] += 1
+            else:
+                resultSum['failures'] += 1
+    if(numTests != str(len(testResults))):
+        resultSum['failures'] += 1
+
+    print("Amount of steps = " + str(len(testResults)) +
+          " (Claimed " + str(numTests) + ")")
+    return testResults, resultSum
+
+# Parse arguments
+
+parser = argparse.ArgumentParser(add_help=True)
+
+parser.add_argument('--infile', nargs='+', required=True,
+                    action='append', dest='inputFiles', default=[])
+parser.add_argument('--outfile', nargs=1, required=True,
+                    dest='outputFile', action='store')
+parser.add_argument('--rootname', nargs=1, required=False,
+                    dest='rootSuiteName', action='store')
+
+parsedArguments = parser.parse_args()
+
+inputFiles = parsedArguments.inputFiles[0]
+outputFile = parsedArguments.outputFile[0]
+
+if(parsedArguments.rootSuiteName):
+    rootSuiteName = parsedArguments.rootSuiteName[0]
+else:
+    rootSuiteName = "Root"
+
+
+rootSuiteName = re.sub('[^A-Za-z0-9\.\-_, ]+', '', rootSuiteName)
+
+print("Will read from files '" + str(inputFiles) + "' and output to '" +
       outputFile + "'")
 print("Using '" + rootSuiteName + "' as test suite root name")
 
-with open(inputFile, "r") as myfile:
-    data = myfile.read()
+# Parse results
+mergedResults = []
+mergedResultsDict = None
+for inputFile in inputFiles:
+    testResults, resultsDict = processRawResultsFile(inputFile)
+    mergedResults += testResults
+    mergedResultsDict = mergedResultsDict or resultsDict
+    for key in resultsDict:
+        print("Checking key '" + key + "'")
+        mergedResultsDict[key] += resultsDict[key]
+    print("Results is a '" + type(testResults).__name__ + "'")
 
-numTests = "-1"
-numTestsLine = -1
-reNumTests = re.compile(r'INSTRUMENTATION_STATUS: numtests=(\d*)$')
-for line in data.splitlines():
-    numTestsLine += 1
-    if(reNumTests.match(line)):
-        numTests = str(reNumTests.search(line).group(1))
-        break
+# Write output
 
-print("DEBUG: Number of lines checked to find numTests = '" + str(numTestsLine) + "'")
-output, bundle = ParseAmInstrumentOutput(data)
-testResults = output
-
-failures = 0
-skipped = 0
-for result in testResults:
-    if(result.GetStatusCode() < 0):
-        if(result.GetStatusCode() == -3):
-            skipped += 1
-        else:
-            failures += 1
-if(numTests != str(len(testResults))):
-    failures += 1
-
-print("Amount of steps = " + str(len(testResults)) +
-      " (Claimed " + str(numTests) + ")")
-
-# add the step that checks that all tests have been executed the the number of
-# total test steps
-totalNumTests = len(testResults) + 1
-
-with open(outputFile, "w") as outfile:
-    outfile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    outfile.write("<testsuites name=\"Unit Tests\" tests=\"" + str(totalNumTests) + "\" failures=\"" + str(failures) + "\" skipped=\"" + str(skipped) + "\"" + ">\n")
-    outfile.write("\t<testsuite name=\"" + rootSuiteName + "\" tests=\"" + str(totalNumTests) + "\" failures=\"" + str(failures) + "\" skipped=\"" + str(skipped) + "\"" + ">\n")
-
-    for result in testResults:
-        outfile.write("\t\t<testcase name=\"" + str(result.GetTestName()) + "\">\n")
-        if(result.GetStatusCode() < 0):
-            if(result.GetStatusCode() == -3):
-                outfile.write("\t\t\t<skipped />\n")
-            else:
-                outfile.write("\t\t\t<failure> <![CDATA[" + str(result.GetFailureReason()) + "]]></failure>\n")
-        outfile.write("\t\t</testcase>\n")
-    outfile.write("\t\t<testcase name=\"All tests were executed\">\n")
-    if(numTests != str(len(testResults))):
-        outfile.write("\t\t\t<failure> <![CDATA[ Expected '" + numTests + "' steps. Got results for '" + str(len(testResults)) + "'" "]]></failure>\n")
-    outfile.write("\t\t</testcase>\n")
-    outfile.write("\t</testsuite>\n")
-    outfile.write("</testsuites>\n")
+writeOutput(outputFile, mergedResults, mergedResultsDict)
 
 print(outputFile + " exported")
